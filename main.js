77a0b3e9a6651825cc1a08935defbb76e8c2af89
Ivan Mitrevski <mitrevski.ivan@hotmail.com>
2021-02-16 21:56:31 +0100

    Popping the balloon when it reaches maxSize

--- main.js
+++ main.js
@@ -2,12 +2,18 @@ let clickCount = 0;
 let height = 120;
 let width = 100;
 let inflationRate = 20;
-
+let maxSize = 300;
 function inflate() {
   clickCount++;
   let balloonElement = document.getElementById("balloon");
   height += inflationRate;
   width += inflationRate;
+
+  if (height >= maxSize) {
+    console.log("pop the balloon")
+    height = 0
+    width = 0
+  }
   balloonElement.style.height = height + "px";
   balloonElement.style.width = width + "px";
 
