c1466fd186a9835bce7346cfef38bff76642b4bb
Ivan Mitrevski <mitrevski.ivan@hotmail.com>
2021-02-16 22:13:14 +0100

    Added a pop counter

--- main.js
+++ main.js
@@ -1,22 +1,27 @@
-let clickCount = 0;
-let height = 120;
-let width = 100;
-let inflationRate = 20;
-let maxSize = 300;
+let clickCount = 0
+let height = 120
+let width = 100
+let inflationRate = 20
+let maxSize = 300
+let popCount = 0
+
 function inflate() {
   clickCount++;
-  let balloonElement = document.getElementById("balloon");
-  height += inflationRate;
-  width += inflationRate;
+  let balloonElement = document.getElementById("balloon")
+  height += inflationRate
+  width += inflationRate
 
   if (height >= maxSize) {
     console.log("pop the balloon")
+    popCount++
     height = 0
     width = 0
+    document.getElementById("pop-count").innerText = popCount.toString();
   }
-  balloonElement.style.height = height + "px";
-  balloonElement.style.width = width + "px";
 
-  let clickCountElement = document.getElementById("click-count");
-  clickCountElement.innerText = clickCount.toString();
+  balloonElement.style.height = height + "px"
+  balloonElement.style.width = width + "px"
+
+  let clickCountElement = document.getElementById("click-count")
+  clickCountElement.innerText = clickCount.toString()
 }
