2a5b62506515f5fbfa1197d238c7dcf84508f4b3
Ivan Mitrevski <mitrevski.ivan@hotmail.com>
2021-02-16 21:36:47 +0100

    The balloon now scales

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 let clickCount = 0;
+let height = 120;
+let width = 100;
+let inflationRate = 20;
 
 function inflate() {
   clickCount++;
   let balloonElement = document.getElementById("balloon");
-  balloonElement.style.height = "20px"
+  height += inflationRate;
+  width += inflationRate;
+  balloonElement.style.height = height + "px";
+  balloonElement.style.width = width + "px";
 
   let clickCountElement = document.getElementById("click-count");
   clickCountElement.innerText = clickCount.toString();
